150aa9ebdf6bd13d0d15290bc9bfc0df6f8d7e83
Bjorn Donald <bjorndonaldb@yahoo.com>
2021-08-09 21:03:54 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Header () {
+  return (
+    <header>
+      <div class='heading'>
+        <h1>
+          Lorem ipsum dolor sit amet,
+          <br /> consectetur adipiscing elit.
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque
+          tincidunt porttitor quam quisque vitae diam cursus.
+        </p>
+        <div class='buttons align-center '>
+          <a href='' class='btn btn-primary'>
+            BOOK A HAIRCUT
+          </a>
+          <a href='' class='btn btn-transparent-white'>
+            SEE BARBER LIST
+          </a>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Nav () {
+  return (
+    <nav>
+      <div class='container row align-center justify-between'>
+        <div class='logo'>
+          H<span>S</span>B
+        </div>
+
+        <div class='hamburger'></div>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
